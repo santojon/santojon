ae0beae15f2f8a96a7e21a13d2b1ba05e442de2a
Jonathan Santos <santojon5@gmail.com>
2021-11-08 10:04:44 -0300

    update some data and UI

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -3,14 +3,10 @@ function updateSmoothScrolling($) {
 
   // Smooth scrolling using jQuery easing
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-    console.log("asdasdasd")
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
-      console.log(target)
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      console.log(2, target)
       if (target.length) {
-        console.log(target.length)
         $('html, body').animate({
           scrollTop: (target.offset().top)
         }, 1000, "easeInOutExpo");
